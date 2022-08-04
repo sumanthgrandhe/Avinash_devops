1274d72065cfcb88825524da741421e64883f5ec
sumanthgrandhe <30905986+sumanthgrandhe@users.noreply.github.com>
2022-08-04 12:32:22 +0530

    Adding project

---
+++ src/main/java/com/example/Avinash_devops/AvinashDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Avinash_devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvinashDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvinashDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Avinash_devops/AvinashDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Avinash_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvinashDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
